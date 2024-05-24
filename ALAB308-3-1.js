182756337b41d1d6b3a44cffbc4976b2f27f296c
Jermal Wynder <jermalwynder@gmail.com>
2024-05-24 10:20:50 -0400

    Rework of solutions

--- ALAB308-3-1.js
+++ ALAB308-3-1.js
@@ -1,20 +1,18 @@
 // Loop through all numbers from 1 to 100
 
 for (let i = 1; i <= 100; i++) {
+  // Check if the number is divisible by both 3 and 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i + " Fizz Buzz");
+  }
   // Check if the number is divisible by 3
-
-  if (i % 3 === 0) {
+  else if (i % 3 === 0) {
     console.log(i + " Fizz");
   }
 
   // Check if the number is divisible by 5
   else if (i % 5 === 0) {
     console.log(i + " Buzz");
-  }
-
-  // Check if the number is divisible by both 3 and 5
-  else if (i % 3 === 0 && i % 5 == 0) {
-    console.log(i + " Fizz Buzz");
   }
 
   // If a number is not divisible by either 3 or 5, log the number
